5ab4668724bd2ba106a03c401d616a67664198b3
LiangYong <liangdongxing.cn@outlook.com>
2023-06-29 17:55:39 +0800

    feat: 主体切换动画逻辑处理

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,39 +1,17 @@
 <template>
   <router-view v-slot="{ Component, route }">
-    <transition :enter-active-class="animante" mode="out-in">
+    <transition name="el-fade-in" mode="out-in">
       <keep-alive :include="tagsViewStore.cachedViews">
         <component :is="Component" v-if="!route.meta.link" :key="route.path" />
       </keep-alive>
     </transition>
   </router-view>
-  <iframe-toggle />
 </template>
 
-<script setup name="AppMain" lang="ts">
+<script setup lang="ts">
 import useTagsViewStore from '@/store/modules/tagsView'
-import useSettingsStore from '@/store/modules/settings'
-import IframeToggle from './IframeToggle/index.vue'
-import { ComponentInternalInstance } from 'vue'
-const { proxy } = getCurrentInstance() as ComponentInternalInstance
-const tagsViewStore = useTagsViewStore()
 
-// 随机动画集合
-const animante = ref<string>('')
-const animationEnable = ref(useSettingsStore().animationEnable)
-watch(
-  () => useSettingsStore().animationEnable,
-  (val) => {
-    animationEnable.value = val
-    if (val) {
-      animante.value = proxy?.animate.animateList[
-        Math.round(Math.random() * proxy?.animate.animateList.length)
-      ] as string
-    } else {
-      animante.value = proxy?.animate.defaultAnimate as string
-    }
-  },
-  { immediate: true }
-)
+const tagsViewStore = useTagsViewStore()
 </script>
 
 <style lang="scss" scoped></style>
--- src/layout1/components/AppMain.vue
+++ src/layout1/components/AppMain.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view v-slot="{ Component, route }">
-    <transition :enter-active-class="animante" mode="out-in">
+    <transition name="el-fade-in" mode="out-in">
       <keep-alive :include="tagsViewStore.cachedViews">
         <component :is="Component" v-if="!route.meta.link" :key="route.path" />
       </keep-alive>
@@ -10,28 +10,8 @@
 
 <script setup lang="ts">
 import useTagsViewStore from '@/store/modules/tagsView'
-import useSettingsStore from '@/store/modules/settings'
-import { ComponentInternalInstance } from 'vue'
-const { proxy } = getCurrentInstance() as ComponentInternalInstance
-const tagsViewStore = useTagsViewStore()
 
-// 随机动画集合
-const animante = ref<string>('')
-const animationEnable = ref(useSettingsStore().animationEnable)
-watch(
-  () => useSettingsStore().animationEnable,
-  (val) => {
-    animationEnable.value = val
-    if (val) {
-      animante.value = proxy?.animate.animateList[
-        Math.round(Math.random() * proxy?.animate.animateList.length)
-      ] as string
-    } else {
-      animante.value = proxy?.animate.defaultAnimate as string
-    }
-  },
-  { immediate: true }
-)
+const tagsViewStore = useTagsViewStore()
 </script>
 
 <style lang="scss" scoped></style>
